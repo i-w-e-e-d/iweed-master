86d595d8a288060916be8ab941daf3aab7cba13b
iweed ™ <44798066+i-w-e-e-d@users.noreply.github.com>
2019-04-05 15:46:05 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# iweed-master
---
+++ iweed-master/src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x44, 0x57, 0x39, 0x50, 0x55, 0x3f, 0x7e, 0x4f, 0x74, 0x41, 0x08, 0x13, 0x11, 0x11, 0x11, 0x10 } };
+}
